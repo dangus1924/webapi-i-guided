aff1b271905eb259ef19257f184a7dfc5ff4dcba
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-07 09:42:32 -0500

    added another get req that show a date stamp

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const server = express();// here is where we call the imported express
 //reques handler
 // the '/' is the route of the request 
 server.get('/', (req, res) => { //req and res is the two hommies think of req as anything incoming and res as anything outgoing.
-        res.send('Hello World') //notice we use res because we are sending a response back to the user that request information from the server. 
-})
-
+        res.send('Hello World'); //notice we use res because we are sending a response back to the user that request information from the server. 
+});
+// notice this new route, when the user goes to localhost:8080/now
+server.get('/now', (req, res) => {
+    const now = new Date().toISOString(); // this is a simple date you can refer to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
+    res.send(now); // just like above we are sending something back because we are using the res.
+});
 
 
 
